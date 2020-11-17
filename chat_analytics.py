0afdac3a01f08111d419801ba49b2d34523a45cc
Farhan <surel.farhan@gmail.com>
2020-11-17 23:51:34 +0700

    words counter per day has been added

--- chat_analytics.py
+++ chat_analytics.py
@@ -40,6 +40,7 @@ class ChatAnalytics:
 
             # ios
             if self.__is_ios:
+                
                 date = i[1:9].strip()
                 time = i[9:15].strip()
                 chat = i[16:]
@@ -48,12 +49,14 @@ class ChatAnalytics:
                 if coord > 0:
                     sender = chat[4:coord].strip()
                     text = chat[(coord + 1):].strip()
-                    chat_dict.append(dict(
+                    chat_item = dict(
                         date=date,
                         time=time,
                         sender=sender,
                         text=text
-                    ))
+                    )
+                    chat_dict.append(chat_item)
+                    
             else:
                 date = i.split(',')[0]
                 time = i[9:15].strip()
@@ -100,23 +103,29 @@ class ChatAnalytics:
         sender_2_wordcount_df = sender_2_wordcount_df[sender_2_wordcount_df.word != 'omitted']
         sender_2_wordcount_df = sender_2_wordcount_df[sender_2_wordcount_df['count'] > 2]
         sender_2_wordcount_df = sender_2_wordcount_df.head(20)
+
+        try:
+            if self.__is_ios:
+                chat_df.date = pd.to_datetime(chat_df.date, format="%d/%m/%y", errors='coerce')
+                chat_df.date = chat_df.date.dt.tz_localize('UTC').dt.tz_convert('Asia/Jakarta')
+            else:
+                chat_df.date = pd.to_datetime(chat_df.date, format="%d/%m/%Y", errors='coerce')
+                chat_df.date = chat_df.date.dt.tz_localize('UTC').dt.tz_convert('Asia/Jakarta')
+        except Exception as e:
+            print(f"faield to covert to datetime format: {e}")
+            
+        chat_df['total_words'] = chat_df.text.apply(lambda x: len(str(x).split()))
+        total_words = chat_df.groupby(['date', 'sender'])['total_words'].sum() \
+            .reset_index() \
+            .rename(columns={'total_words': 'total'})
         
+        total_words = total_words.sort_values(by='date')
 
         chat_df['?'] = chat_df['text'].str.contains("""[?]""").astype(int)
         qmarks_counter = chat_df.groupby(['date', 'sender'])['?'].sum()\
             .reset_index()\
             .rename(columns={'?': 'total'})
-
-        qmarks_counter = qmarks_counter[qmarks_counter.date.str.split('/').str.len() >= 3]
-
-        try:
-            if self.__is_ios:
-                qmarks_counter.date = pd.to_datetime(qmarks_counter.date, format="%d/%m/%y")
-            else:
-                qmarks_counter.date = pd.to_datetime(qmarks_counter.date, format="%d/%m/%Y")
-        except Exception as e:
-            print(f"faield to covert to datetime format: {e}")
-
+        
         qmarks_counter = qmarks_counter.sort_values(by='date')
 
 
@@ -128,18 +137,47 @@ class ChatAnalytics:
 
         fig = plt.figure("Chat Analytics")
         
-        
+        # axes
         ax_word_count_sender_1 = fig.add_axes((0.3, 0.08, 0.15, 0.8))
         ax_word_count_sender_2 = fig.add_axes((0.08, 0.08, 0.15, 0.8))
-        ax_counter = fig.add_axes((0.52, 0.17, 0.45, 0.45))
+        ax_qmarks = fig.add_axes((0.52, 0.15, 0.45, 0.3))
+        ax_qmarks.set_title('? Counter')
+        ax_qmarks.set_ylabel('total ?')
         
-        df = qmarks_counter.pivot(index='date', columns='sender', values='total')
-        df = df.fillna(0)
+        ax_total_words = fig.add_axes((0.52, 0.58, 0.45, 0.3))
+        ax_total_words.tick_params(
+                        axis='x',          # changes apply to the x-axis
+                        which='both',      # both major and minor ticks are affected
+                        bottom=False,      # ticks along the bottom edge are off
+                        top=False,         # ticks along the top edge are off
+                        labelbottom=False)
+        ax_total_words.set_title('Words')
+        ax_total_words.set_ylabel('total words')
+        ax_total_words.set_xlabel('date')
+        
+        
+        qmarks_pivot = qmarks_counter.pivot(index='date', columns='sender', values='total')
+        qmarks_pivot = qmarks_pivot.fillna(0)
 
-        spearman_corr = round(df.iloc[:,[0, 1]].corr('spearman').iloc[:, 0][1], 2)
-        df.iloc[:,0].plot(grid=True, label=df.iloc[:,0].name, legend=True, ax=ax_counter, figsize=(15, 7))
-        df.iloc[:,1].plot(grid=True, label=df.iloc[:,1].name, legend=True, ax=ax_counter, figsize=(15, 7))
+        spearman_corr_qmarks = round(qmarks_pivot.iloc[:,[0, 1]].corr('spearman').iloc[:, 0][1], 2)
+        qmarks_pivot.iloc[:,0].plot(grid=True, label=qmarks_pivot.iloc[:,0].name, legend=True, ax=ax_qmarks, figsize=(15, 7))
+        qmarks_pivot.iloc[:,1].plot(grid=True, label=qmarks_pivot.iloc[:,1].name, legend=True, ax=ax_qmarks, figsize=(15, 7))
         
+        # total words plot
+        total_words_pivot = total_words.pivot(index='date', columns='sender', values='total')
+        total_words_pivot = total_words_pivot.fillna(0)
+        total_words_pivot.iloc[:, 0].plot(grid=True, 
+                                          label=total_words_pivot.iloc[:, 0].name, legend=True,
+                                          figsize=(15, 7),
+                                          ax=ax_total_words,
+                                          x=None)
+        total_words_pivot.iloc[:, 1].plot(grid=True, 
+                                          label=total_words_pivot.iloc[:, 1].name, legend=True,
+                                          figsize=(15, 7),
+                                          ax=ax_total_words,
+                                          x=None)
+        
+        # word count
         sender_1_wordcount_df.sort_values(by='count').plot.barh(x='word', 
                                                                 y='count', 
                                                                 ax=ax_word_count_sender_1,
@@ -156,9 +194,7 @@ class ChatAnalytics:
                                                                 xlabel='',
                                                                 title=sender_2)
 
-        plt.title('? counter')
-        plt.ylabel('? count')
-        fig.text(.85, .63, f"spearman corr: {spearman_corr}", ha='left')
+        fig.text(.85, .46, f"spearman corr: {spearman_corr_qmarks}", ha='left')
         plt.show()
 
 @click.command()
